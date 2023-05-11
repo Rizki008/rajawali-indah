704cd05316a33c74790476e22c3a07d68f0c09c3
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-05-11 15:50:49 +0700

    tambah library

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'form_validation', 'upload', 'database');
+$autoload['libraries'] = array('session', 'form_validation', 'upload', 'database', 'login_user');
 
 /*
 | -------------------------------------------------------------------
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('m_home');
+$autoload['model'] = array('m_auth');
