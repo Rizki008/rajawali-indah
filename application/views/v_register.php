4ba61ba952fec5a28f73b383dee06d7dd8d7feb8
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-05-11 15:52:15 +0700

    view register

---
+++ application/views/v_register.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<!-- Basic Page Info -->
+	<meta charset="utf-8">
+	<title><?= $title ?></title>
+
+	<!-- Site favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= base_url() ?>deskapp-master/vendors/images/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?= base_url() ?>deskapp-master/vendors/images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?= base_url() ?>deskapp-master/vendors/images/favicon-16x16.png">
+
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>deskapp-master/vendors/styles/core.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>deskapp-master/vendors/styles/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>deskapp-master/vendors/styles/style.css">
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-119386393-1');
+	</script>
+</head>
+
+<body class="login-page">
+	<div class="login-header box-shadow">
+		<div class="container-fluid d-flex justify-content-between align-items-center">
+			<div class="brand-logo">
+				<!-- <a href="login.html">
+					<img src="<?= base_url() ?>deskapp-master/vendors/images/deskapp-logo.svg" alt="">
+				</a> -->
+			</div>
+			<div class="login-menu">
+				<ul>
+					<li><a href="<?= base_url('auth/login') ?>">Login</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="login-wrap d-flex align-items-center flex-wrap justify-content-center">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-md-6 col-lg-7">
+					<img src="<?= base_url() ?>deskapp-master/vendors/images/login-page-img.png" alt="">
+				</div>
+				<div class="col-md-6 col-lg-5">
+					<div class="login-box bg-white box-shadow border-radius-10">
+						<div class="login-title">
+							<h2 class="text-center text-primary">Login To Sistem</h2>
+						</div>
+						<?php
+						echo validation_errors('<div class="alert alert-warning alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <h5><i class="icon fa fa-exclamation-triangle"></i> Coba Lagi</h5>', '</div>');
+
+						if ($this->session->flashdata('pesan')) {
+							echo '<div class="alert alert-success alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <h5><i class="icon fa fa-check"></i> Sukses</h5>';
+							echo $this->session->flashdata('pesan');
+							echo '</div>';
+						}
+
+						?>
+						<form action="<?= base_url('auth/register') ?>" method="post">
+							<div class="input-group custom">
+								<input type="text" class="form-control form-control-lg" name="nama_user" value="<?= set_Value('nama_user') ?>" placeholder="Name Supplier">
+								<div class="input-group-append custom">
+									<span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
+								</div>
+							</div>
+							<div class="input-group custom">
+								<input type="text" class="form-control form-control-lg" name="username" value="<?= set_Value('username') ?>" placeholder="Username">
+								<div class="input-group-append custom">
+									<span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
+								</div>
+							</div>
+							<div class="input-group custom">
+								<input type="password" class="form-control form-control-lg" name="password" value="<?= set_Value('password') ?>" placeholder="**********">
+								<div class="input-group-append custom">
+									<span class="input-group-text"><i class="dw dw-padlock1"></i></span>
+								</div>
+							</div>
+							<div class="input-group custom">
+								<input type="password" class="form-control form-control-lg" name="ulangi_password" value="<?= set_value('ulangi_password') ?>" placeholder="**********">
+								<div class="input-group-append custom">
+									<span class="input-group-text"><i class="dw dw-padlock1"></i></span>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-12">
+									<div class="input-group mb-0">
+										<!--
+											use code for form submit
+											<input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">
+										-->
+										<button type="submit" class="btn btn-primary btn-lg btn-block">Register</button>
+									</div>
+									<div class="font-16 weight-600 pt-10 pb-10 text-center" data-color="#707373">OR</div>
+									<div class="input-group mb-0">
+										<a class="btn btn-outline-primary btn-lg btn-block" href="<?= base_url('auth/login') ?>">Login</a>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- js -->
+	<script src="<?= base_url() ?>deskapp-master/vendors/scripts/core.js"></script>
+	<script src="<?= base_url() ?>deskapp-master/vendors/scripts/script.min.js"></script>
+	<script src="<?= base_url() ?>deskapp-master/vendors/scripts/process.js"></script>
+	<script src="<?= base_url() ?>deskapp-master/vendors/scripts/layout-settings.js"></script>
+</body>
+
+</html>
