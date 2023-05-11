a1c0a253f40257e13891d502f561c1714faf2ab4
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-05-11 15:53:30 +0700

    library login

---
+++ application/libraries/Login_user.php
@@ -0,0 +1,66 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login_User
+{
+	protected $ci;
+
+
+	public function __construct()
+	{
+		$this->ci = &get_instance();
+		$this->ci->load->model('m_auth');
+	}
+
+	public function login($username, $password)
+	{
+		$cek = $this->ci->m_auth->login_user($username, $password);
+		if ($cek) {
+			$id_user = $cek->id_user;
+			$nama_user = $cek->nama_user;
+			$username = $cek->username;
+			$password = $cek->password;
+			$status = $cek->status;
+			$level_user = $cek->level_user;
+
+			$this->ci->session->set_userdata('id_user', $id_user);
+			$this->ci->session->set_userdata('nama_user', $nama_user);
+			$this->ci->session->set_userdata('username', $username);
+			$this->ci->session->set_userdata('password', $password);
+			$this->ci->session->set_userdata('status', $status);
+			$this->ci->session->set_userdata('level_user', $level_user);
+
+			if ($level_user === '1') {
+				redirect('admin');
+			} elseif ($level_user === '2') {
+				redirect('pemilik');
+			} elseif ($level_user === '3') {
+				redirect('supplier');
+			}
+		} else {
+			$this->ci->session->set_flashdata('error', 'Username Atau Password Salah');
+			redirect('auth/login');
+		}
+	}
+
+	public function proteksi_halaman()
+	{
+		if ($this->ci->session->userdata('username') == '') {
+			$this->ci->session->set_flashdata('error', 'Anda Belum Login');
+			redirect('auth/login');
+		}
+	}
+
+	public function logout()
+	{
+		$this->ci->session->unset_userdata('id_user');
+		$this->ci->session->unset_userdata('nama_user');
+		$this->ci->session->unset_userdata('username');
+		$this->ci->session->set_userdata('password');
+		$this->ci->session->set_userdata('status');
+		$this->ci->session->unset_userdata('level_user');
+		$this->ci->session->set_flashdata('pesan', 'Berhasil LogOut!!!!');
+		redirect('auth/login');
+	}
+}
