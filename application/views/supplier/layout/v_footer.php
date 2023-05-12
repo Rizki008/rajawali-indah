dd0703184d23df8e2e029162cad79bc6101d61f8
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-05-12 14:45:31 +0700

    view supplier

---
+++ application/views/supplier/layout/v_footer.php
@@ -0,0 +1,19 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+	Toko Mebel <a href="#" target="_blank">Indah Jaya</a>
+</div>
+</div>
+</div>
+<!-- js -->
+<script src="<?= base_url() ?>deskapp-master/vendors/scripts/core.js"></script>
+<script src="<?= base_url() ?>deskapp-master/vendors/scripts/script.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/vendors/scripts/process.js"></script>
+<script src="<?= base_url() ?>deskapp-master/vendors/scripts/layout-settings.js"></script>
+<script src="<?= base_url() ?>deskapp-master/src/plugins/apexcharts/apexcharts.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/jquery.dataTables.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/dataTables.responsive.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
+<script src="<?= base_url() ?>deskapp-master/vendors/scripts/dashboard3.js"></script>
+</body>
+
+</html>
