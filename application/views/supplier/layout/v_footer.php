56228545e064176cb2008d69445862f1d832100d
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-05-13 20:02:42 +0700

    ubah footer supplier

--- application/views/supplier/layout/v_footer.php
+++ application/views/supplier/layout/v_footer.php
@@ -9,11 +9,24 @@
 <script src="<?= base_url() ?>deskapp-master/vendors/scripts/process.js"></script>
 <script src="<?= base_url() ?>deskapp-master/vendors/scripts/layout-settings.js"></script>
 <script src="<?= base_url() ?>deskapp-master/src/plugins/apexcharts/apexcharts.min.js"></script>
+
 <script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/jquery.dataTables.min.js"></script>
 <script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/dataTables.bootstrap4.min.js"></script>
 <script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/dataTables.responsive.min.js"></script>
 <script src="<?= base_url() ?>deskapp-master/src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
+
 <script src="<?= base_url() ?>deskapp-master/vendors/scripts/dashboard3.js"></script>
+
+<script>
+	console.log = function() {}
+	$("#pesan_barang").on('change', function() {
+
+		$(".name").html($(this).find(':selected').attr('data-name'));
+		$(".name").val($(this).find(':selected').attr('data-name'));
+	});
+</script>
+
+
 </body>
 
 </html>
