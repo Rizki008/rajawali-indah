fce260ef7a3030f6b4a24059211a3b50f60b84bc
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-05-13 21:58:27 +0700

    informasi supplier

---
+++ application/controllers/Supplier.php
@@ -0,0 +1,41 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Supplier extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		//Load Dependencies
+
+	}
+
+	// List all your items
+	public function index()
+	{
+		$data = array(
+			'title' => 'Indah Jaya Mebeul',
+			'isi' => 'supplier/v_supplier'
+		);
+		$this->load->view('supplier/layout/v_wrapper', $data, FALSE);
+	}
+
+	// Add a new item
+	public function add()
+	{
+	}
+
+	//Update one item
+	public function update($id = NULL)
+	{
+	}
+
+	//Delete one item
+	public function delete($id = NULL)
+	{
+	}
+}
+
+/* End of file Admin.php */
