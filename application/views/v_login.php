32215d00dd63445d095bd2f703720434aa5849af
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-05-13 22:32:25 +0700

    menambahkan informasi error

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -66,6 +66,15 @@
                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                     <h5><i class="icon fa fa-exclamation-triangle"></i> Coba Lagi</h5>', '</div>');
 
+						if ($this->session->flashdata('error')) {
+							echo '<div class="alert alert-danger alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <h5><i class="icon fa fa-ban"></i> Gagal</h5>';
+							echo $this->session->flashdata('error');
+							echo '
+    </div>';
+						}
+
 						if ($this->session->flashdata('pesan')) {
 							echo '<div class="alert alert-success alert-dismissible">
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
