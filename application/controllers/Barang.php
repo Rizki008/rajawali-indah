943706354fb9e028f3cb2cf8006f1609a7e72913
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-05-13 22:00:13 +0700

    crud barang

---
+++ application/controllers/Barang.php
@@ -0,0 +1,163 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Barang extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_barang');
+		$this->load->model('m_user');
+		$this->load->model('m_kategori');
+	}
+
+	// List all your items
+	public function index()
+	{
+		$data = array(
+			'title' => 'Kelola Barang',
+			'barang' => $this->m_barang->barang(),
+			'isi' => 'gudang/barang/v_barang'
+		);
+		$this->load->view('gudang/layout/v_wrapper', $data, FALSE);
+	}
+
+	public function add()
+	{
+		$this->form_validation->set_rules('nama_barang', 'Nama Barang', 'required', array('required' => '%s Mohon untuk diisi!!!'));
+		$this->form_validation->set_rules('id_kategori', 'Kategori Barang', 'required', array('required' => '%s Mohon untuk diisi!!!'));
+		$this->form_validation->set_rules('id_user', 'Supplier Barang', 'required', array('required' => '%s Mohon untuk diisi!!!'));
+		$this->form_validation->set_rules('stock', 'Stock Barang', 'required', array('required' => '%s Mohon untuk diisi!!!'));
+		$this->form_validation->set_rules('deskripsi', 'Deskripsi Barang', 'required', array('required' => '%s Mohon untuk diisi!!!'));
+
+		if ($this->form_validation->run() == TRUE) {
+			$config['upload_path'] = './assets/barang';
+			$config['allowed_types'] = 'gif|jpg|png|jpeg';
+			$config['max_size']  = '5000';
+			$this->upload->initialize($config);
+			$field_name = "gambar";
+			if (!$this->upload->do_upload($field_name)) {
+				$data = array(
+					'title' => 'Tambah data barang',
+					'kategori' => $this->m_kategori->kategori(),
+					'user' => $this->m_user->user_supplier(),
+					'error_upload' => $this->upload->display_errors(),
+					'isi' => 'gudang/barang/v_add'
+				);
+				$this->load->view('gudang/layout/v_wrapper', $data, FALSE);
+			} else {
+				$upload_data = array('uploads' => $this->upload->data());
+				$config['image_library'] = 'gd2';
+				$config['source_image'] = './assets/barang' . $upload_data['uploads']['file_name'];
+				$this->load->library('image_lib', $config);
+				$data = array(
+					'nama_barang' => $this->input->post('nama_barang'),
+					'id_kategori' => $this->input->post('id_kategori'),
+					'id_user' => $this->input->post('id_user'),
+					'stock' => $this->input->post('stock'),
+					'deskripsi' => $this->input->post('deskripsi'),
+					'gambar' => $upload_data['uploads']['file_name'],
+				);
+				$this->m_barang->add($data);
+				$this->session->set_flashdata('pesan', 'Data barang berhasil ditambahkan');
+				redirect('barang');
+			}
+		}
+		$data = array(
+			'title' => 'Tambah data barang',
+			'kategori' => $this->m_kategori->kategori(),
+			'user' => $this->m_user->user_supplier(),
+			'isi' => 'gudang/barang/v_add'
+		);
+		$this->load->view('gudang/layout/v_wrapper', $data, FALSE);
+	}
+	public function update($id_barang = NULL)
+	{
+		$this->form_validation->set_rules('nama_barang', 'Nama Barang', 'required', array('required' => '%s Mohon untuk diisi!!!'));
+		$this->form_validation->set_rules('id_kategori', 'Kategori Barang', 'required', array('required' => '%s Mohon untuk diisi!!!'));
+		$this->form_validation->set_rules('id_user', 'Supplier Barang', 'required', array('required' => '%s Mohon untuk diisi!!!'));
+		$this->form_validation->set_rules('stock', 'Stock Barang', 'required', array('required' => '%s Mohon untuk diisi!!!'));
+		$this->form_validation->set_rules('deskripsi', 'Deskripsi Barang', 'required', array('required' => '%s Mohon untuk diisi!!!'));
+
+		if ($this->form_validation->run() == TRUE) {
+			$config['upload_path'] = './assets/barang';
+			$config['allowed_types'] = 'gif|jpg|png|jpeg';
+			$config['max_size']  = '5000';
+			$this->upload->initialize($config);
+			$field_name = "gambar";
+			if (!$this->upload->do_upload($field_name)) {
+				$data = array(
+					'title' => 'Tambah data barang',
+					'kategori' => $this->m_kategori->kategori(),
+					'user' => $this->m_user->user_supplier(),
+					'details' => $this->m_barang->details($id_barang),
+					'error_upload' => $this->upload->display_errors(),
+					'isi' => 'gudang/barang/v_edit'
+				);
+				$this->load->view('gudang/layout/v_wrapper', $data, FALSE);
+			} else {
+				//hapus gambar di folder
+				$produk = $this->m_barang->details($id_barang);
+				if ($produk->gambar !== "") {
+					unlink('./assets/barang/' . $produk->gambar);
+				}
+				//end
+				$upload_data = array('uploads' => $this->upload->data());
+				$config['image_library'] = 'gd2';
+				$config['source_image'] = './assets/barang' . $upload_data['uploads']['file_name'];
+				$this->load->library('image_lib', $config);
+				$data = array(
+					'id_barang' => $id_barang,
+					'nama_barang' => $this->input->post('nama_barang'),
+					'id_kategori' => $this->input->post('id_kategori'),
+					'id_user' => $this->input->post('id_user'),
+					'stock' => $this->input->post('stock'),
+					'deskripsi' => $this->input->post('deskripsi'),
+					'gambar' => $upload_data['uploads']['file_name'],
+				);
+				$this->m_barang->update($data);
+				$this->session->set_flashdata('pesan', 'Data barang berhasil ditambahkan');
+				redirect('barang');
+			}
+			$data = array(
+				'id_barang' => $id_barang,
+				'nama_barang' => $this->input->post('nama_barang'),
+				'id_kategori' => $this->input->post('id_kategori'),
+				'id_user' => $this->input->post('id_user'),
+				'stock' => $this->input->post('stock'),
+				'deskripsi' => $this->input->post('deskripsi'),
+			);
+			$this->m_barang->update($data);
+			$this->session->set_flashdata('pesan', 'Data barang berhasil ditambahkan');
+			redirect('barang');
+		} else {
+			$data = array(
+				'title' => 'Tambah data barang',
+				'kategori' => $this->m_kategori->kategori(),
+				'user' => $this->m_user->user_supplier(),
+				'details' => $this->m_barang->details($id_barang),
+				'isi' => 'gudang/barang/v_edit'
+			);
+			$this->load->view('gudang/layout/v_wrapper', $data, FALSE);
+		}
+	}
+
+	public function delete($id_barang = null)
+	{
+		$barang = $this->m_barang->details($id_barang);
+		if ($barang->gambar !== "") {
+			unlink('./assets/gambar/' . $barang->gambar);
+		}
+
+		$data = array(
+			'id_barang' => $id_barang,
+		);
+		$this->m_barang->delete($data);
+		$this->session->set_flashdata('pesan', 'Barang berhasil dihapus!!!');
+		redirect('barang');
+	}
+}
+
+/* End of file kontrol_barang.php */
